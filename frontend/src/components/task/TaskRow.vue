09a7f3670e1e077e14ff40112bb81dcf4f9cad5c
r4ya1n <luzini2007@gmail.com>
2026-05-23 11:46:04 +0500

    bug fix

--- frontend/src/components/task/TaskRow.vue
+++ frontend/src/components/task/TaskRow.vue
@@ -4,7 +4,7 @@
      border border-border
      hover:bg-bg3 cursor-pointer">
         <div class="col-span-2 flex items-center gap-5">
-            <div class="" @click="toogleDoneTask">
+            <div class="" @click="toggleDoneTask">
                 <div
                 v-if="task.isDone"
                 class="bg-teal w-5 h-5
@@ -17,7 +17,7 @@
             <div>
                 <h3 class="text-sm" :class="{'line-through': task.isDone, 'text-gray': task.isDone}">{{ task.name }}</h3>
                 <div class="flex items-center gap-3">
-                    <TaskTagPill v-for="tag in task.tags" :tag="tag"></TaskTagPill>
+                    <TaskTagPill v-for="tag in task.tags" :key="tag" :tag="tag"></TaskTagPill>
                 </div>
             </div>
         </div>
@@ -66,12 +66,9 @@ const tasks = inject("tasks")
 const statusMeta = computed(() => TASK_STATUS[task.status.toLowerCase().trim()]) 
 const priorityMeta = computed(() => TASK_PRIORITY[task.priority.toLowerCase().trim()]) 
 
-const toogleDoneTask = function() {
-    if (!task.isDone) {
-        tasks.value[idx].old_status = task.status
-    }
+const toggleDoneTask = function() {
     task.isDone = !task.isDone
-    tasks.value[idx].status = task.isDone ? "done" : tasks.value[idx].old_status;
+    tasks.value[idx].status = task.isDone ? "done" : "in_progress"
 }
 </script>
 
