db4a26a60385cd0d1bad62d30305c811b5d2d4f8
r4ya1n <luzini2007@gmail.com>
2026-07-06 17:59:38 +0500

    mice

--- frontend/src/types/task.ts
+++ frontend/src/types/task.ts
@@ -2,6 +2,31 @@ import type { Component } from "vue"
 import type { Member } from "./project"
 import type { Color } from "@/constants/colors"
 
+export type Status = "TODO" | "IN_PROGRESS" | "IN_REVIEW" | "DONE"
+export type Priority = "LOW" | "MEDIUM" | "HIGH" | "URGENT"
+
+export interface ICheckItem {
+    id?: number,
+    name: string,
+    isDone: boolean
+}
+
+export interface TaskListItem {
+    id: number
+    tags: string[]
+    title: string
+    desription: string
+    executor: Member
+    status: Status
+    priority: Priority
+    deadline: Date
+}
+
+export interface Task extends TaskListItem {
+    author: Member,
+    checkItems: ICheckItem[]
+}
+
 export interface IOptionMeta {
     label: string,
     icon: Component,
@@ -27,27 +52,4 @@ export interface ICreateTaskForm {
     checkItems: ICheckItem[]
 }
 
-export type Status = "TODO" | "IN_PROGRESS" | "IN_REVIEW" | "DONE"
-export type Priority = "LOW" | "MEDIUM" | "HIGH" | "URGENT"
 
-export interface ICheckItem {
-    id?: number,
-    name: string,
-    isDone: boolean
-}
-
-export interface TaskListItem {
-    id: number
-    tags: string[]
-    title: string
-    desription: string
-    executor: Member
-    status: Status
-    priority: Priority
-    deadline: Date
-}
-
-export interface Task extends TaskListItem {
-    author: Member,
-    checkItems: ICheckItem[]
-}
